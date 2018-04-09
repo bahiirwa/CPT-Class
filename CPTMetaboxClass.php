b5760491744689d1c638e42b32c6bc4fa1d9c36a
Laurence Bahiirwa <laurence@mediauganda.com>
2018-04-09 15:02:34 +0300

    text domain variable addition

--- CPTMetaboxClass.php
+++ CPTMetaboxClass.php
@@ -9,11 +9,13 @@ class custom_taxonomy_class
 
  private $taxo_name;
  private $CPT_name;
+ private $textdomain;
 
- public function __construct( $taxo_name, $CPT_name )
+ public function __construct( $taxo_name, $CPT_name, $textdomain )
  {
     $this->taxo_name = $taxo_name;
     $this->CPT_name = strtolower( $CPT_name );
+    $this->taxo_name = $textdomain;
 
     add_action( 'init', array($this, 'register_custom_fields_taxonomy'), 0 );
 
@@ -22,17 +24,17 @@ class custom_taxonomy_class
  public function register_custom_fields_taxonomy() {
 
      $labels = array(
-       'name' => _x( $this->taxo_name . ' Categories', 'taxonomy general name' ),
-       'singular_name' => _x( $this->taxo_name . ' Category', 'taxonomy singular name' ),
-       'search_items' =>  __( 'Search ' . $this->taxo_name . ' Categories' ),
-       'all_items' => __( 'All ' . $this->taxo_name . ' Categories' ),
-       'parent_item' => __( 'Parent ' . $this->taxo_name . ' Category' ),
-       'parent_item_colon' => __( 'Parent ' . $this->taxo_name . ' Category:' ),
-       'edit_item' => __( 'Edit ' . $this->taxo_name . ' Category' ),
-       'update_item' => __( 'Update ' . $this->taxo_name . ' Category' ),
-       'add_new_item' => __( 'Add New ' . $this->taxo_name . ' Category' ),
-       'new_item_name' => __( 'New ' . $this->taxo_name . ' Category Name' ),
-       'menu_name' => __( $this->taxo_name . ' Categories' ),
+       'name' => _x( $this->taxo_name . ' Categories', 'taxonomy general name', $this->textdomain ),
+       'singular_name' => _x( $this->taxo_name . ' Category', 'taxonomy singular name', $this->textdomain ),
+       'search_items' =>  __( 'Search ' . $this->taxo_name . ' Categories', $this->textdomain ),
+       'all_items' => __( 'All ' . $this->taxo_name . ' Categories', $this->textdomain ),
+       'parent_item' => __( 'Parent ' . $this->taxo_name . ' Category', $this->textdomain ),
+       'parent_item_colon' => __( 'Parent ' . $this->taxo_name . ' Category:', $this->textdomain ),
+       'edit_item' => __( 'Edit ' . $this->taxo_name . ' Category', $this->textdomain ),
+       'update_item' => __( 'Update ' . $this->taxo_name . ' Category', $this->textdomain ),
+       'add_new_item' => __( 'Add New ' . $this->taxo_name . ' Category', $this->textdomain ),
+       'new_item_name' => __( 'New ' . $this->taxo_name . ' Category Name', $this->textdomain ),
+       'menu_name' => __( $this->taxo_name . ' Categories', $this->textdomain ),
      );
 
    // Now register the taxonomy
